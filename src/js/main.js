34f4154e4417e448efa88264c8f04b8acdaa0a3a
Jovanica.K <jovanica0312@gmail.com>
2023-07-25 13:19:16 +0200

    Added header with responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,29 @@
-var $nav = $('.nav');
-
 function navigation() {
-  var $toggle = $('.toggle, .toggle--close');
-  var $main = $('main, footer');
+  var $nav = $('.nav');
+  var $navlink = $('.nav-link');
+  var active = ('nav-link--active');
+  var $toggle = $('.header__toggle');
 
-  $toggle.on('click', function(){
-    $nav.animate({
-      width: 'toggle'
-    });
-    $main.toggleClass('opacity');
+  $navlink.on('click', function() {
+    $navlink.removeClass(active);
+    $(this).addClass(active);
+  })
+
+  $toggle.on('click', function() {
+    $nav.slideToggle();
   });
+
   $(window).on('scroll', function() {
     if($(this).height()>500) {
       $nav.removeAttr('style');
-      $main.removeClass('opacity');
     }
   })
-  
+
+  $(window).on('resize', function() {
+    if ($(this).width()>767) {
+      $nav.removeAttr('style');
+    }
+  })
 }
 
 navigation();
